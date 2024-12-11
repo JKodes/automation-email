a8e99c1c798c15ccc34fa8f2a4c51457aa815d06
JKodes <adrainford09@gmail.com>
2024-12-11 02:33:20 -0500

    add a subject an a message for the sender

--- email.py
+++ email.py
@@ -1,4 +1,13 @@
 import smtplib
 
+sender_email = 'testautoemail@gmail.com'
+password = 'pasword'
 
-connection = smtplib.SMTP('smtp.gmail.com')
+with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+    
+    connection.starttls()  
+    connection.login(user=sender_email, password=password)
+    connection.sendmail(
+        from_addr=sender_email
+        to_addrs=sender_email
+        msg='Subject:Hello Developer...\n\nDue to budget cuts we have to let go some people soon please get ready for the up incoming weeks if you dont see someone in the office you know why most likely dropped form the team')
