036b1ea3e2c16f6182e83166d3a249259905c504
JKodes <adrainford09@gmail.com>
2024-12-27 03:30:42 -0500

    add subject and body

--- email.py
+++ email.py
@@ -8,6 +8,6 @@ with smtplib.SMTP('smtp.gmail.com', 587) as connection:
     connection.starttls()  
     connection.login(user=sender_email, password=password)
     connection.sendmail(
-        from_addr=sender_email
-        to_addrs=sender_email
-        msg='Subject:Hello Developer...\n\nDue to budget cuts we have to let go some people soon please get ready for the up incoming weeks if you dont see someone in the office you know why most likely dropped form the team')
+        from_addr=sender_email,
+        to_addrs=sender_email,
+        msg='Subject:Hello Developers...\n\nDue to budget cuts we have to let go some people soon please get ready for the up incoming weeks if you dont see someone in the office you know why most likely dropped form the team')
